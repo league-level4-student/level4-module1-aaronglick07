ca2e157f19f9294c4587e2789d6bc125a03686e7
League Student <league@iMac-14.attlocal.net>
2019-08-17 14:28:55 -0700

    case switch stuff
    today I was exteremly productive as one can see by looking at the freaking amazing comit to github...in this comit we can see that I did some work on case and switches which is an infior version of if/else if statments which we all know are a supior version of case and switch statements.   Anywho my next comit wont be for a while because I wont be here next week and who knows about any week after that.  In that case it has been fun most of the time
    
    cool, cool, cool, cool, cool.  No doubt, no doubt, no doubt

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,15 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
-		
+		switch(choice) {
+			case "Sunday": System.out.println("it is sunday"); break;
+			case "Monday": System.out.println("it is monday"); break;
+			case "Tuesday": System.out.println("it is tuesday"); break;
+			case "Wednesday": System.out.println("it is wednesday"); break;
+			case "Thursday": System.out.println("it is thursday"); break;
+			case "Friday": System.out.println("it is friday"); break;
+			case "Saturday": System.out.println("it is saturday"); break;
+			default : System.out.println("it is today"); break;
+		}
 	}
 }
